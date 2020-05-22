0a19cb464ccdc8bdff06dcb47e9d3738887aaa0d
Ananda Luiza <anandanemesio@gmail.com>
2020-05-21 21:26:38 -0300

    Criando classe principal e implementando método push

--- src/algoritmos/pilha/Pilha.java
+++ src/algoritmos/pilha/Pilha.java
@@ -1,7 +1,17 @@
 package algoritmos.pilha;
 
 public class Pilha {
-    public static void main (String[] args){
-        System.out.println("Hello World");
+    int [] v1 = new int [3];
+    int top = 0;
+
+    public void push(int elemento){
+        if(top<=v1.length) {
+            v1[top] = elemento;
+            System.out.println(v1[0] + ","+ v1[1] + "," + v1[2]);
+        }
+        top++;
+    }
+    public int pop(){
+        return 0;
     }
 }
---
+++ src/algoritmos/pilha/PilhaTeste.java
@@ -0,0 +1,10 @@
+package algoritmos.pilha;
+
+public class PilhaTeste {
+    public static void main (String[] args){
+        Pilha pilha1 = new Pilha();
+        pilha1.push(5);
+        pilha1.push(7);
+        pilha1.push(2);
+    }
+}
