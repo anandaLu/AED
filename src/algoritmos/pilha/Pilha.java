b5576c6ab28b7a2b9fc39f57a28a250de9ee0056
Ananda Luiza <anandanemesio@gmail.com>
2020-05-21 21:36:57 -0300

    Adicionando comentários ao código

--- src/algoritmos/pilha/Pilha.java
+++ src/algoritmos/pilha/Pilha.java
@@ -1,17 +1,32 @@
 package algoritmos.pilha;
 
+//Criando classe pilha
 public class Pilha {
+
+    //Vetor de inteiros com tamanho 3
     int [] v1 = new int [3];
+
+    //Ponteiro
     int top = 0;
 
+    //Método de inserção no vetor (pede como parâmetro um elemento inteiro
     public void push(int elemento){
+
+        //Checando se o valor de top é maior ou igual que o tamanho de v1
         if(top<=v1.length) {
+
+            //Adicionando o elemento a v1 na posição atual do ponteiro
             v1[top] = elemento;
+
+            //Imprimindo os valores armazenados em v1
             System.out.println(v1[0] + ","+ v1[1] + "," + v1[2]);
         }
+        //Incrementado o valor do ponteiro
         top++;
     }
+    //Método para remover os elementos do vetor
     public int pop(){
+
         return 0;
     }
 }
--- src/algoritmos/pilha/PilhaTeste.java
+++ src/algoritmos/pilha/PilhaTeste.java
@@ -1,8 +1,15 @@
 package algoritmos.pilha;
 
+//Classe teste (Principal)
 public class PilhaTeste {
+
+    //Método de execução dos programas
     public static void main (String[] args){
+
+        //Inicializando um objeto que aponta para a classe Pilha
         Pilha pilha1 = new Pilha();
+
+        //Adicionando elementos ao vetor através do método push
         pilha1.push(5);
         pilha1.push(7);
         pilha1.push(2);
