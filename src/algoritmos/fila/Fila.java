0080eb26007d871b88f6745036054def685f2107
Ananda Luiza <anandanemesio@gmail.com>
2020-05-22 16:03:04 -0300

    Fila com método "Inserir" funcionando

--- src/algoritmos/fila/Fila.java
+++ src/algoritmos/fila/Fila.java
@@ -2,13 +2,39 @@ package algoritmos.fila;
 
 public class Fila {
 
+    //Criando um vetor de String que irá armazenar todos os elementos
     String[] fila = new String[4];
-    int cabeca;
+
+    //ponteiro resposável pelo ínicio da fila (Começando em 0)
+    int cabeca = 0;
+
+    //ponteiro responsável pelo fim da fila
     int cauda;
 
-    public void inserir(){
+    //Método para inserir elementos na fila
+    public void inserir(String elemento){
 
-    }
+        //Informa que é permitido inserir elementos
+        System.out.println("Fila está vazia, adicione elementos.");
+
+        //Checa se a cabeca é menor que a fila
+        if(cabeca<fila.length) {
+                //Adiciona um elemento na posição da cabeça
+                fila[cabeca] = elemento;
+
+                //Incrementa a cabeca para a próxima posição vazia
+                cabeca++;
+
+                //Imprime a fila
+                System.out.println(fila[0] + "," + fila[1] + "," + fila[2] + "," + fila[3]);
+            }
+        //Checa se o tamanho da fila é igual ao ponteiro
+        if(cabeca==fila.length) {
+                //Informa que a fila está cheia
+                System.out.println("Fila está cheia! impossível adicionar mais elementos.");
+            }
+        }
+        //Método responsável por remover elementos da pilha
     public void remover(){
 
     }
--- src/algoritmos/fila/FilaTeste.java
+++ src/algoritmos/fila/FilaTeste.java
@@ -1,7 +1,19 @@
 package algoritmos.fila;
 
+//Classe principal
 public class FilaTeste {
+
+    //Método de execução
     public static void main(String[] args){
+
+        //Inicializando um objeto que aponta para a classe Fila
         Fila fila1 = new Fila();
+
+        //Adicionando elementos à fila
+        fila1.inserir("Maria");
+        fila1.inserir("Mateus");
+        fila1.inserir("Júnior");
+        fila1.inserir("Thiago");
+        fila1.inserir("João");
     }
 }
